a1c9d76b47b93a21c035184a1156223b1915ca77
Ricardo Alvarez Diaz <sproplayerxd@gmail.com>
2023-05-04 20:49:38 +0200

    🎨➕Translate API added & separate CSS file

--- form.js
+++ form.js
@@ -1,3 +1,27 @@
+
+
+
+// API translation
+const data = 'q=English%20is%20hard%2C%20but%20detectably%20so';
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener('readystatechange', function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});
+
+xhr.open('POST', 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect');
+xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+xhr.setRequestHeader('Accept-Encoding', 'application/gzip');
+xhr.setRequestHeader('X-RapidAPI-Key', '3d5ce6dfb4mshaeeea817946dc73p17c499jsnd9561d07c12a');
+xhr.setRequestHeader('X-RapidAPI-Host', 'google-translate1.p.rapidapi.com');
+
+xhr.send(data);
+
+// BULMA JS form
 function stripe() {
     var stripe = Stripe(
         "pk_test_Kk44etb9arfGs64zFUGivUPZ"
@@ -10,8 +34,6 @@ for (const key in productsData) {
       break;
     }
 }
-
-      
       console.log(productId);
 
         stripe.redirectToCheckout({
